eee7c166fa0aca1e3d980329a5a29ce8c9c543ae
lcs9317 <l65783082@gmail.com>
2025-07-01 12:30:05 +0900

    wiseSaying page

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,16 @@
+export default function Page() {
+  return <>
+  <h1>명언</h1>
+  <ul>
+    <li>
+        명언 1
+    </li>
+    <li>
+        명언 2
+    </li>
+    <li>
+        명언 3
+    </li>
+    </ul>
+  </>;
+}
