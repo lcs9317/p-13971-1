f1e4edd4b428fd6ab1d9812366d5c9cf6a3d628c
lcs9317 <l65783082@gmail.com>
2025-07-01 14:57:18 +0900

    connect api server

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -3,19 +3,19 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 export default function Page() {
 
-    const [posts, setPosts] = useState<{ [key: string]: any }[]>([]);
+    const [posts, setPosts] = useState([{id: 0, title: '', content: ''}]);
 
     useEffect(() => {
-        setPosts([
-            {id: 1, title: '명언 1', content: '명언 1 내용'},
-            {id: 2, title: '명언 2', content: '명언 2 내용'},
-            {id: 3, title: '명언 3', content: '명언 3 내용'},
-        ]);
+        
+        fetch('http://localhost:3000/api/v1/posts')
+        .then(res => res.json())
+        .then(setPosts);
     }, []);
 
   return (<>
   <h1>글 목록</h1>
   <ul>
+    {posts.length == 0 && <div>로딩중...</div>}
     {posts.map((post) => (
         <li key={post.id}>
             <Link href={`/posts/${post.id}`}>{post.title}</Link>
